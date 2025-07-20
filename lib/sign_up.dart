f3693f0545603aa06a708417119bbdd85fe88175
Neelakandan Nampoothiri N <neelakandannampoothirin@gmail.com>
2025-07-20 13:21:15 +0530

    new

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -8,153 +8,157 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 140, right: 10, left: 10),
-            decoration: BoxDecoration(),
-            height: 600,
-            width: 500,
-            child: Column(
-              children: [
-                Text(
-                  "Sign Up",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: const Color.fromARGB(255, 116, 15, 163),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Container(
-                  height: 10,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 116, 15, 163),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                    top: 80,
-                    bottom: 0,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "User Name",
-                      suffixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurple,
-                          width: 2.0,
-                          style: BorderStyle.solid,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 140, right: 10, left: 10),
+                decoration: BoxDecoration(),
+                height: 600,
+                width: 500,
+                child: Column(
+                  children: [
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: const Color.fromARGB(255, 116, 15, 163),
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                    top: 35,
-                    bottom: 0,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      suffixIcon: Icon(Icons.mail),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurple,
-                          width: 2.0,
-                          style: BorderStyle.solid,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
+                    SizedBox(height: 5),
+                    Container(
+                      height: 10,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 116, 15, 163),
+                        borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                    top: 35,
-                    bottom: 0,
-                  ),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 80,
+                        bottom: 0,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "User Name",
+                          suffixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2.0,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 35,
+                        bottom: 0,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          suffixIcon: Icon(Icons.mail),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2.0,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 35,
+                        bottom: 0,
+                      ),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
 
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurple,
-                          width: 2.0,
-                          style: BorderStyle.solid,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2.0,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 50),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(
-                      const Color.fromARGB(255, 61, 1, 103),
-                    ),
-                    minimumSize: WidgetStatePropertyAll(Size(350, 50)),
-                    padding: WidgetStatePropertyAll(
-                      EdgeInsets.only(top: 15, bottom: 15),
-                    ),
-                    elevation: WidgetStatePropertyAll(5),
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(20),
+                    SizedBox(height: 50),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          const Color.fromARGB(255, 61, 1, 103),
+                        ),
+                        minimumSize: WidgetStatePropertyAll(Size(350, 50)),
+                        padding: WidgetStatePropertyAll(
+                          EdgeInsets.only(top: 15, bottom: 15),
+                        ),
+                        elevation: WidgetStatePropertyAll(5),
+                        shape: WidgetStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(20),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
 
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginPage()),
-                    );
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.deepPurple,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/start_page.dart
+++ lib/start_page.dart
@@ -7,72 +7,76 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 600,
-            margin: EdgeInsets.only(top: 90, right: 10, left: 10),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 600,
+                margin: EdgeInsets.only(top: 90, right: 10, left: 10),
 
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
                   children: [
-                    SizedBox(height: 120),
-                    Image.network(
-                      "https://m.media-amazon.com/images/I/615o19jQZjL.png",
-                      scale: 10,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 120),
+                        Image.network(
+                          "https://m.media-amazon.com/images/I/615o19jQZjL.png",
+                          scale: 10,
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          "BookIt CEC",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 47, 2, 56),
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 15),
-                    Text(
-                      "BookIt CEC",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 47, 2, 56),
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
+                    Image.asset("images/booking image.png"),
                   ],
                 ),
-                Image.asset("images/booking image.png"),
-              ],
-            ),
-          ),
-          SizedBox(height: 160),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(
-                const Color.fromARGB(255, 61, 1, 103),
               ),
-              minimumSize: WidgetStatePropertyAll(Size(350, 50)),
-              padding: WidgetStatePropertyAll(
-                EdgeInsets.only(top: 15, bottom: 15),
-              ),
-              elevation: WidgetStatePropertyAll(5),
-              shape: WidgetStatePropertyAll(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(20),
+              SizedBox(height: 160),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                    const Color.fromARGB(255, 61, 1, 103),
+                  ),
+                  minimumSize: WidgetStatePropertyAll(Size(350, 50)),
+                  padding: WidgetStatePropertyAll(
+                    EdgeInsets.only(top: 15, bottom: 15),
+                  ),
+                  elevation: WidgetStatePropertyAll(5),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(20),
+                    ),
+                  ),
+                ),
+
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpPage()),
-              );
-            },
-            child: Text(
-              "Get Started",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
